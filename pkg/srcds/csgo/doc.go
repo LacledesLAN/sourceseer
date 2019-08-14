b1704837f4a460d087f0cbb95b348a22dab4f577
james dudley <jedudley@outlook.com>
2019-08-13 20:50:45 -0500

    documentation tweak

--- pkg/srcds/csgo/doc.go
+++ pkg/srcds/csgo/doc.go
@@ -1,5 +1,5 @@
 /*
-Package csgo provides functionality for tailing, parsing, and interpreting source dedicated server logging along with constructs
+Package csgo provides functionality for tailing, parsing, and interpreting <a href="https://blog.counter-strike.net/">Counter-Strike: Global Offensive</a> dedicated server logging along with constructs
 for automating wrapped, executing srcds processes.
 */
 package csgo
