7a393b21526fb437068f67b079a9e78f8fc12b6b
james dudley <jedudley@outlook.com>
2019-08-26 20:13:49 -0500

    flushing out wrapping

--- pkg/srcds/server.go
+++ pkg/srcds/server.go
@@ -3,13 +3,18 @@ package srcds
 import (
 	"bufio"
 	"context"
+	"fmt"
+	"io"
+	"os"
 	"os/exec"
+	"os/signal"
 	"strings"
+	"syscall"
 	"time"
 )
 
-// RefreshCvars attempts to trigger SRCDS into echoing all watched cvars to the log stream.
-func (s *Server) RefreshCvars() {
+// RefreshWatchedCvars triggers SRCDS into echoing all watched cvars to the log stream.
+func (s *Server) RefreshWatchedCvars() {
 	go func(s *Server) {
 		for _, name := range s.cvars.getNames() {
 			s.SendCommand(name)
@@ -18,52 +23,101 @@ func (s *Server) RefreshCvars() {
 	}(s)
 }
 
-// SendCommand to the interactive SRCDS instance
+// SendCommand to the interactive SRCDS instance.
 func (s *Server) SendCommand(cmd string) {
-
+	c := strings.TrimSpace(cmd)
+	if len(c) > 0 {
+		s.cmdIn <- c
+	}
 }
 
-// Command for observing and interacting with a SRCDS instance
-func Command(name string, osArgs ...string) (*Server, error) {
-	ctx, _ := context.WithCancel(context.Background())
+// Command returns the Server struct for launching and wrapping a SRCDS instance.
+func Command(name string, arg ...string) (*Server, error) {
+	ctx, cancel := context.WithCancel(context.Background())
 
-	cmd := exec.CommandContext(ctx, name, osArgs...)
+	go func(cancel context.CancelFunc) {
+		s := make(chan os.Signal, 2)
+		signal.Notify(s, syscall.SIGINT, syscall.SIGTERM)
+		lastSigInt := time.Time{}
 
-	return wrapProcess(cmd)
+		for sig := range s {
+			if sig == syscall.SIGINT && time.Since(lastSigInt) > time.Second*8 {
+				fmt.Fprintf(os.Stderr, "Press ctrl^c again in the next 8 seconds to terminate.")
+				lastSigInt = time.Now()
+				continue
+			}
+
+			fmt.Fprintf(os.Stderr, "Terminating process.")
+			cancel()
+			time.Sleep(2 * time.Second)
+			os.Exit(0)
+		}
+	}(cancel)
+
+	return CommandContext(ctx, name, arg...)
+}
+
+// CommandContext is like command but includes a context.
+func CommandContext(ctx context.Context, name string, arg ...string) (*Server, error) {
+	cmd := exec.CommandContext(ctx, name, arg...)
+
+	stdOut, err := cmd.StdoutPipe()
+	if err != nil {
+		return nil, err
+	}
+
+	cmdStdIn, err := cmd.StdinPipe()
+	if err != nil {
+		return nil, err
+	}
+
+	server := &Server{
+		cmdIn:    make(chan string, 6),
+		observer: *newReader(stdOut),
+	}
+
+	go func(wc io.WriteCloser, s *Server) {
+		prev := time.Time{}
+		ticker := time.NewTicker(90 * time.Millisecond)
+		defer wc.Close()
+		defer ticker.Stop()
+
+		for {
+			select {
+			case <-ticker.C:
+				// Send EOL to flush the process's standard out buffer
+				if time.Since(prev) >= 100*time.Millisecond {
+					prev = time.Now()
+					wc.Write([]byte(s.endOfLine))
+				}
+			case cmd := <-server.cmdIn:
+				// Send the command to process's standard in
+				prev = time.Now()
+				wc.Write([]byte(cmd + s.endOfLine))
+			case <-ctx.Done():
+				// Politely ask SRCDS to shutdown
+				wc.Write([]byte("say server shutting down" + s.endOfLine))
+				wc.Write([]byte("quit" + s.endOfLine))
+				time.Sleep(500 * time.Millisecond)
+				return
+			}
+		}
+	}(cmdStdIn, server)
+
+	go func(r io.ReadCloser) {
+		defer r.Close()
+		s := bufio.NewScanner(r)
+
+		for s.Scan() {
+			server.cmdIn <- s.Text()
+		}
+	}(os.Stdin)
+
+	return server, nil
 }
 
 // Server represents an interactive SRCDS instance
 type Server struct {
 	cmdIn chan string
 	observer
-}
-
-func wrapProcess(cmd *exec.Cmd) (*Server, error) {
-	stdOut, err := cmd.StdoutPipe()
-	if err != nil {
-		return nil, err
-	}
-
-	//if stdErr, err := cmd.StderrPipe(); err == nil {
-	//// TODO read and process standard error
-	//}
-
-	return &Server{
-		cmdIn:    make(chan string, 9),
-		observer: *newReader(stdOut),
-	}, nil
-}
-
-func (s *Server) linkStdOut(r *bufio.Reader) {
-	go func(r *bufio.Reader) {
-		for {
-			outLine, _ := r.ReadString('\n')
-			outLine = strings.Trim(strings.TrimSuffix(outLine, "\n"), "")
-
-			if len(outLine) > 0 {
-
-				Log(SRCDSOther, outLine)
-			}
-		}
-	}(r)
 }
