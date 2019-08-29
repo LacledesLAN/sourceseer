6503f4223736e12912f5ea033c21fcc3b29ea33d
Nathan Cowen <cowen.nathan@gmail.com>
2019-08-29 16:28:50 -0500

    Update doc.go

--- pkg/srcds/csgo/doc.go
+++ pkg/srcds/csgo/doc.go
@@ -1,5 +1,5 @@
 /*
 Package csgo provides functionality for tailing, parsing, and interpreting <a href="https://blog.counter-strike.net/">Counter-Strike: Global Offensive</a> dedicated server logging along with constructs
-for automating wrapped, executing srcds processes.
+for automating wrapped, executing csgo server processes.
 */
 package csgo
